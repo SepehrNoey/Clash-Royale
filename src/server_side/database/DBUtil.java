978ba6d614b0c96aee5b5a5153d2dd0591cbbc29
Ali Noroozbeigy <77932230+Ali-Noroozbeigy@users.noreply.github.com>
2021-07-10 20:43:35 +0430

    database connection for initializing players

--- src/server_side/database/DBUtil.java
+++ src/server_side/database/DBUtil.java
@@ -1,11 +1,19 @@
 package server_side.database;
 
+import shared.enums.CardTypes;
+import shared.enums.SpeedTypes;
+import shared.enums.TargetTypes;
+import shared.model.card.BuildingCard;
+import shared.model.card.Card;
+import shared.model.card.SoldierCard;
+import shared.model.card.SpellCard;
 import shared.model.player.Player;
 
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.sql.*;
+import java.util.ArrayList;
 
 public class DBUtil {
 
@@ -31,15 +39,97 @@ public class DBUtil {
             {
                 if (resultSet.getString("username").equals(usr) && resultSet.getString("password").equals(password))
                 {
-                    // new the cards then player then return
+                    ArrayList<Card> cards = new ArrayList<>();
+                    Card card;
+                    String tableName = usr +" cards";
+                    ResultSet cardsSet = connection.createStatement().executeQuery("SELECT * FROM "+tableName);
+
+                    cardsSet.next();
+                    card = new SoldierCard(CardTypes.valueOf(cardsSet.getString("type")),5,cardsSet.getString("damage")
+                    ,cardsSet.getString("level"),,,,,,0, TargetTypes.GROUND,4,false, SpeedTypes.MEDIUM,1.5
+                    ,cardsSet.getString("hp"),,,,);
+                    cards.add(card);
+
+                    cardsSet.next();
+                    card = new SoldierCard(CardTypes.valueOf(cardsSet.getString("type")),3,cardsSet.getString("damage")
+                            ,cardsSet.getString("level"),,,,,,5, TargetTypes.AIR_GROUND,2,false, SpeedTypes.MEDIUM,1.2
+                            ,cardsSet.getString("hp"),,,,);
+                    cards.add(card);
+
+                    cardsSet.next();
+                    card = new SoldierCard(CardTypes.valueOf(cardsSet.getString("type")),4,cardsSet.getString("damage")
+                            ,cardsSet.getString("level"),,,,,,3, TargetTypes.AIR_GROUND,1,true, SpeedTypes.FAST,1.8
+                            ,cardsSet.getString("hp"),,,,);
+                    cards.add(card);
+
+                    cardsSet.next();
+                    card = new SoldierCard(CardTypes.valueOf(cardsSet.getString("type")),5,cardsSet.getString("damage")
+                            ,cardsSet.getString("level"),,,,,,5, TargetTypes.AIR_GROUND,1,true, SpeedTypes.MEDIUM,1.7
+                            ,cardsSet.getString("hp"),,,,);
+                    cards.add(card);
+
+                    cardsSet.next();
+                    card = new SoldierCard(CardTypes.valueOf(cardsSet.getString("type")),4,cardsSet.getString("damage")
+                            ,cardsSet.getString("level"),,,,,,0, TargetTypes.GROUND,1,false, SpeedTypes.FAST,1.8
+                            ,cardsSet.getString("hp"),,,,);
+                    cards.add(card);
+
+                    cardsSet.next();
+                    card = new SoldierCard(CardTypes.valueOf(cardsSet.getString("type")),5,cardsSet.getString("damage")
+                            ,cardsSet.getString("level"),,,,,,0, TargetTypes.BUILDINGS,1,false, SpeedTypes.SLOW,1.5
+                            ,cardsSet.getString("hp"),,,,);
+                    cards.add(card);
+
+                    cardsSet.next();
+                    card = new SoldierCard(CardTypes.valueOf(cardsSet.getString("type")),4,cardsSet.getString("damage")
+                            ,cardsSet.getString("level"),,,,,,0, TargetTypes.GROUND,1,true, SpeedTypes.MEDIUM,1.5
+                            ,cardsSet.getString("hp"),,,,);
+                    cards.add(card);
+
+                    cardsSet.next();
+                    card = new SpellCard(CardTypes.valueOf(cardsSet.getString("type")),3,cardsSet.getString("damage")
+                    ,cardsSet.getString("level"),,,,,,5,null,1,null,null
+                            ,40,40,40,false);
+                    cards.add(card);
+
+                    // reduce damage?
+                    cardsSet.next();
+                    card = new SpellCard(CardTypes.valueOf(cardsSet.getString("type")),4,cardsSet.getString("damage")
+                            ,cardsSet.getString("level"),,,,,,2.5,null,1,null,null
+                            ,0,0,0,true);
+                    cards.add(card);
+
+                    cardsSet.next();
+                    card = new SpellCard(CardTypes.valueOf(cardsSet.getString("type")),3,cardsSet.getString("damage")
+                            ,cardsSet.getString("level"),,,,,,4,null,1,null,null
+                            ,0,0,0,true);
+                    cards.add(card);
+
+                    cardsSet.next();
+                    card = new BuildingCard(CardTypes.valueOf(cardsSet.getString("type")),6,cardsSet.getString("damage")
+                    ,cardsSet.getString("level"),,,,,,5.5,TargetTypes.GROUND,1,false,,,
+                            0.8,30,cardsSet.getString("hp"));
+                    cards.add(card);
+
+                    cardsSet.next();
+                    card = new BuildingCard(CardTypes.valueOf(cardsSet.getString("type")),5,cardsSet.getString("damage")
+                            ,cardsSet.getString("level"),,,,,,6,TargetTypes.AIR_GROUND,1,false,,,
+                            0.4,40,cardsSet.getString("hp"));
+                    cards.add(card);
+
+                    return new Player(usr,password,Integer.parseInt(resultSet.getString("level")),Integer.parseInt(resultSet.getString("xp")),
+                            cards,socket,outObj,inObj);
+
                 }
             }
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
+        return null;
     }
 
-    public static Player register(String usr, String password)
+    public static Player register(String usr, String password, Socket socket, ObjectOutputStream outObj,
+                                  ObjectInputStream inObj)
     {
         StringBuilder sb = new StringBuilder();
         sb.append("('");sb.append(usr);sb.append("','");sb.append(password);sb.append("',1,0)");
@@ -50,36 +140,98 @@ public class DBUtil {
             sb.setLength(0);
             sb.append("CREATE TABLE ");
             sb.append(tableName);
-            sb.append("(type VARCHAR(255), cost INT, level INT, damage INT, hp INT, PRIMARY KEY(type))");
+            sb.append("(type VARCHAR(255), level INT, damage INT, hp INT, PRIMARY KEY(type))");
 
+            ArrayList<Card> cards = new ArrayList<>();
+            Card card;
             Statement cardSt = connection.createStatement();
             cardSt.executeUpdate(sb.toString());
-            cardSt.executeUpdate("INSERT INTO "+tableName+" (type,cost,level,damage,hp) VALUES" +
-                    " ('CardTypes.BARBARIAN',5,1,75,300)");
-            cardSt.executeUpdate("INSERT INTO "+tableName+" (type,cost,level,damage,hp) VALUES" +
-                    " ('CardTypes.ARCHER',3,1,33,125)");
-            cardSt.executeUpdate("INSERT INTO "+tableName+" (type,cost,level,damage,hp) VALUES" +
-                    " ('CardTypes.BABY_DRAGON',4,1,100,800)");
-            cardSt.executeUpdate("INSERT INTO "+tableName+" (type,cost,level,damage,hp) VALUES" +
-                    " ('CardTypes.WIZARD',5,1,130,340)");
-            cardSt.executeUpdate("INSERT INTO "+tableName+" (type,cost,level,damage,hp) VALUES" +
-                    " ('CardTypes.MINI_PEKKA',4,1,325,600)");
-            cardSt.executeUpdate("INSERT INTO "+tableName+" (type,cost,level,damage,hp) VALUES" +
-                    " ('CardTypes.GIANT',5,1,126,2000)");
-            cardSt.executeUpdate("INSERT INTO "+tableName+" (type,cost,level,damage,hp) VALUES" +
-                    " ('CardTypes.VALKYRIE',4,1,120,880)");
-            cardSt.executeUpdate("INSERT INTO "+tableName+" (type,cost,level,damage,hp) VALUES" +
-                    " ('CardTypes.RAGE',3,1,0,6)");
-            cardSt.executeUpdate("INSERT INTO "+tableName+" (type,cost,level,damage,hp) VALUES" +
-                    " ('CardTypes.FIREBALL',4,1,325,0)");
-            cardSt.executeUpdate("INSERT INTO "+tableName+" (type,cost,level,damage,hp) VALUES" +
-                    " ('CardTypes.ARROWS',3,1,144,0)");
-            cardSt.executeUpdate("INSERT INTO "+tableName+" (type,cost,level,damage,hp) VALUES" +
-                    " ('CardTypes.CANNON',6,1,60,380)");
-            cardSt.executeUpdate("INSERT INTO "+tableName+" (type,cost,level,damage,hp) VALUES" +
-                    " ('CardTypes.INFERNO_TOWER',5,1,400,800)");
+            cardSt.executeUpdate("INSERT INTO "+tableName+" (type,level,damage,hp) VALUES" +
+                    " ('CardTypes.BARBARIAN',1,75,300)");
+            card = new SoldierCard(CardTypes.BARBARIAN,5,75,1,,,,,,0, TargetTypes.GROUND,4,
+                    false, SpeedTypes.MEDIUM,1.5,300,,,,);
+            cards.add(card);
 
-            String tableName = usr+" history";
+
+            cardSt.executeUpdate("INSERT INTO "+tableName+" (type,level,damage,hp) VALUES" +
+                    " ('CardTypes.ARCHER',1,33,125)");
+            card = new SoldierCard(CardTypes.ARCHER,3,33,1,,,,,,5, TargetTypes.AIR_GROUND,2,
+                    false, SpeedTypes.MEDIUM,1.2,125,,,,);
+            cards.add(card);
+
+
+            cardSt.executeUpdate("INSERT INTO "+tableName+" (type,level,damage,hp) VALUES" +
+                    " ('CardTypes.BABY_DRAGON',1,100,800)");
+            card = new SoldierCard(CardTypes.BABY_DRAGON,4,100,1,,,,,,3, TargetTypes.AIR_GROUND,1,
+                    true, SpeedTypes.FAST,1.8,800,,,,);
+            cards.add(card);
+
+
+            cardSt.executeUpdate("INSERT INTO "+tableName+" (type,level,damage,hp) VALUES" +
+                    " ('CardTypes.WIZARD',1,130,340)");
+            card = new SoldierCard(CardTypes.WIZARD,5,130,1,,,,,,5, TargetTypes.AIR_GROUND,1,
+                    true, SpeedTypes.MEDIUM,1.7,340,,,,);
+            cards.add(card);
+
+
+            cardSt.executeUpdate("INSERT INTO "+tableName+" (type,level,damage,hp) VALUES" +
+                    " ('CardTypes.MINI_PEKKA',1,325,600)");
+            card = new SoldierCard(CardTypes.MINI_PEKKA,4,325,1,,,,,,0, TargetTypes.GROUND,1,
+                    false, SpeedTypes.FAST,1.8,600,,,,);
+            cards.add(card);
+
+
+            cardSt.executeUpdate("INSERT INTO "+tableName+" (type,level,damage,hp) VALUES" +
+                    " ('CardTypes.GIANT',1,126,2000)");
+            card = new SoldierCard(CardTypes.GIANT,5,126,1,,,,,,0, TargetTypes.BUILDINGS,1,
+                    false, SpeedTypes.SLOW,1.5,2000,,,,);
+            cards.add(card);
+
+
+            cardSt.executeUpdate("INSERT INTO "+tableName+" (type,level,damage,hp) VALUES" +
+                    " ('CardTypes.VALKYRIE',1,120,880)");
+            card = new SoldierCard(CardTypes.VALKYRIE,4,120,1,,,,,,0, TargetTypes.GROUND,1,
+                    true, SpeedTypes.MEDIUM,1.5,880,,,,);
+            cards.add(card);
+
+
+            cardSt.executeUpdate("INSERT INTO "+tableName+" (type,level,damage,hp) VALUES" +
+                    " ('CardTypes.RAGE',1,0,6)");
+            card = new SpellCard(CardTypes.RAGE,3,0,1,,,,,,5,null,1,null,null
+                    ,40,40,40,false);
+            cards.add(card);
+
+
+            cardSt.executeUpdate("INSERT INTO "+tableName+" (type,level,damage,hp) VALUES" +
+                    " ('CardTypes.FIREBALL',1,325,0)");
+            card = new SpellCard(CardTypes.FIREBALL,4,325,1,,,,,,2.5,null,1,null,null
+                    ,0,0,0,true);
+            cards.add(card);
+
+
+            cardSt.executeUpdate("INSERT INTO "+tableName+" (type,level,damage,hp) VALUES" +
+                    " ('CardTypes.ARROWS',1,144,0)");
+            card = new SpellCard(CardTypes.ARROWS,3,144,1,,,,,,4,null,1,null,null
+                    ,0,0,0,true);
+            cards.add(card);
+
+
+            cardSt.executeUpdate("INSERT INTO "+tableName+" (type,level,damage,hp) VALUES" +
+                    " ('CardTypes.CANNON',1,60,380)");
+            card = new BuildingCard(CardTypes.CANNON,6,60,1,,,,,,5.5,TargetTypes.GROUND,1,false,,,
+                    0.8,30,380);
+            cards.add(card);
+
+
+            cardSt.executeUpdate("INSERT INTO "+tableName+" (type,level,damage,hp) VALUES" +
+                    " ('CardTypes.INFERNO_TOWER',1,400,800)");
+            card = new BuildingCard(CardTypes.INFERNO_TOWER,5,400,1,,,,,,6,TargetTypes.AIR_GROUND,1,false,,,
+                    0.4,40,800);
+            cards.add(card);
+
+
+
+            tableName = usr+" history";
             sb.setLength(0);
             sb.append("CREATE TABLE ");
             sb.append(tableName);
@@ -87,6 +239,8 @@ public class DBUtil {
             statement.executeUpdate(sb.toString());
 
             // new the cards and make the player and return
+            return new Player(usr,password,1,0, cards,socket,outObj,inObj);
+
 
         } catch (SQLException throwables) {
             throwables.printStackTrace();
